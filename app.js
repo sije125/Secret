cd195b39d457be1386d5b2e9112794bcbff113b9
CJ Chairez <sije125@gmail.com>
2022-07-13 20:15:41 -0500

    Add files via upload
    practice authenticating with hashing and salting using bcrypt

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
-console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -38,24 +38,28 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save((err) => {
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.render("secrets");
+      }
+    })
   });
 
-  newUser.save((err) => {
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.render("secrets");
-    }
-  })
 });
 
 app.post("/login", (req,res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, (err, foundUser) => {
     if(err) {
@@ -63,9 +67,12 @@ app.post("/login", (req,res) => {
     }
     else {
       if(foundUser) {
-        if(foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if(result === true) {
+            res.render("secrets");
+          }
+        });
+
       }
     }
   })
